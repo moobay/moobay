fe07552a4744c0b8ba276183188e431e3288c36e
moobay <moobay@foxmail.com>
2017-01-03 20:17:32 +0800

    optimize post images

--- assets/mitmproxy/crop.sh
+++ assets/mitmproxy/crop.sh
@@ -3,5 +3,5 @@ cd $(dirname ${0})
 find raw -iname '*.png' | while read png
 do
 	name=$(echo ${png} | awk -F'/' '{print $NF}')
-	convert ${png} -fuzz 50% -trim -scale 50% -verbose ${name}
+	convert ${png} -fuzz 50% -trim -scale 840 -verbose ${name}
 done
